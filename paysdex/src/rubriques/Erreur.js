73225b83125b5ae6f451c7a61e844956e6e36e00
GUNDUZ_Maxime_Ziya <ziyagunduz10@gmail.com>
2022-04-10 20:25:10 +0200

    aldous
    Signed-off-by: GUNDUZ_Maxime_Ziya <ziyagunduz10@gmail.com>

---
+++ paysdex/src/rubriques/Erreur.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Erreur = () => {
+    return (
+        <center>
+            <div>
+               <p>Erreur 404, l'élément n'a pas étais trouver,
+                veuillez mettre un chemain valide dans l'URL</p>
+                <img src="./erreur-404-2.jpg" alt="erreur404" height="300px" weight="300px" />
+            </div>
+        </center>
+    );
+};
+
+export default Erreur;
+   
